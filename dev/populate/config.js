16f31a1f38fe720b34e5f26c5b28e9dc0aa7cbee
Matteo De Pauw <matteodepauw@yahoo.com>
2023-11-10 11:09:11 +0100

    fix(config): fixed env

--- dev/populate/config.js
+++ dev/populate/config.js
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: "../../.env.local" });
+
 export const defaultDataPoints = 100;
 
 export const sqlProfile = {
     host: process.env.DB_HOST,
-    user: process.env.USER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 }
